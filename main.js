685ff6ffd9b0b7d006c2809cd7476e3a3630095f
Demetrio Turchetta <Kensir96@gmail.com>
2022-01-23 18:47:25 +0100

    Ridotta formula per multipli di 15

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const containerElement = document.getElementById("container");
 
 for (let i = 1; i <= 100; i++) {
 
-    if (i%3 == 0 && i%5 == 0) {
+    if (i%15 == 0) {
         containerElement.innerHTML = containerElement.innerHTML + "<div class='box multiplo3_5'>FizzBuzz</div>"
     }
     else if (i%3 == 0) {
